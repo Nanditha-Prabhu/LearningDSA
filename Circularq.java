dc5f19d859a8effd5ffa5be91fcee693f3088f73
Nandithu <nanditha7prabhu@gmail.com>
2023-03-12 00:53:37 +0530

    mistakes

--- Circularq.java
+++ Circularq.java
@@ -1,31 +1,49 @@
-public class Circularq
-    int a[];
+import java.util.*;
+
+public class Circularq {
+    int[] a = new int[20] ;
     int n, front, rear;
-    public Circularq(int n){
+    Circularq(int n){
         this.n = n;
-        this.a = new int[n];
+        this.front = -1;
+        this.rear = -1;
     }
     void enqueue(int data){
-        if((rear+1)%n == front){
+        if(((this.rear)+1)%(n) == (this.front)){
             return;
         }
-        if(front == -1){
-            front = 0;
-            rear = (rear+1)%n;
-            a[rear] = data;
+        if(this.front == -1){
+            this.front = 0;
+            this.rear = ((this.rear)+1)%(n);
+            this.a[this.rear] = data;
         }
     }
     int dequeue(){
-        if(front == -1){
+        if(this.front == -1){
             System.out.println("Exception");
         }
-        int result = a[front];
-        if(front == rear){
-            front = rear = -1;
+        int result = this.a[this.front];
+        if(this.front == this.rear){
+            this.front = this.rear = -1;
         }
         else{
-            front = (front+1)%n;
+            this.front = ((this.front)+1)%(n);
         }
         return result;
+    }
+    public static void main(String[] args){
+        Circularq c = new Circularq(5);
+        Scanner s = new Scanner(System.in);
+        for(int i=0; i<(this.n); i++){
+            c.a[i] = s.nextInt();
+        }
+        s.close();
+        int m = c.dequeue();
+        System.out.println(m);
+        c.enqueue(11);
+        c.enqueue(12);
+        for(int i=0; i<this.n; i++){
+            System.out.println(c.a[i]);
+        }
     }
 }
